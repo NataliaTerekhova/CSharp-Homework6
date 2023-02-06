4602d0c7558a89b11f1e8efb3b9ae45eee719b06
NataliaTerekhova <121491406+NataliaTerekhova@users.noreply.github.com>
2023-02-06 10:28:30 +0300

    Add files via upload
    Added Homework 6

---
+++ Homework6/Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+
+System.Console.WriteLine("Введите количество чисел: ");
+int amount = Convert.ToInt32 (Console.ReadLine());
+int[] arr = GetArray(amount);
+//PrintArray(arr);
+int result = GetSumOfPositiveNum (arr);
+System.Console.WriteLine( $"В массиве {result} чисел больше 0");
+
+
+
+int [] GetArray(int length)
+{
+int[] array = new int[length];
+
+   for (int i=0; i<length; i++)
+    {
+
+        System.Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    
+    }
+    return array; 
+}
+
+//void PrintArray (int[]arr)
+// {
+//     foreach (int item in arr)
+//     {
+//         System.Console.Write(item + ", ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+int GetSumOfPositiveNum(int[]array)
+{
+    int count=0;
+    for(int i=0; i<array.Length; i++)
+    {
+       
+        if (array[i] >0) count++;
+    }
+    return count;
+}
+
+    
+    
+
+    
+
+
+
+
---
+++ Homework6/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+
+double b1= new Random().Next(1,11);
+double k1= new Random().Next(1,11);
+double b2= new Random().Next(1,11);
+double k2= new Random().Next(1,11);
+
+System.Console.WriteLine( $"b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+
+double x = (b2-b1)/(k1-k2);
+double y =(k1*x) + b1;
+
+//(k1*x) + b1= (k2*x) + b2;
+ 
+System.Console.WriteLine( $"x={x}, y={y}");
+
+if (x==y)
+{
+    System.Console.WriteLine("Прямые пересекаются в этой точке");
+}
+else 
+{
+System.Console.WriteLine("Прямые не пересекаются в этой точке");
+}
